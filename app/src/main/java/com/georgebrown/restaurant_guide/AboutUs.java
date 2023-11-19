f3bf65794575874c76e652831ed12f57b9f6f762
RitzRey <reyritz2224@gmail.com>
2023-11-19 18:26:46 -0500

    About Us and Review Interface class and xml files, strings.xml file modified

---
+++ app/src/main/java/com/georgebrown/restaurant_guide/AboutUs.java
@@ -0,0 +1,30 @@
+package com.georgebrown.restaurant_guide;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_us);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
