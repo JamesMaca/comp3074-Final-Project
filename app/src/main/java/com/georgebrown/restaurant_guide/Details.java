14634886110b18c67d941670db60725d9ae75d5b
James MacAloney <121572130+JamesMaca@users.noreply.github.com>
2023-11-19 17:53:29 -0500

    finished details page

--- app/src/main/java/com/georgebrown/restaurant_guide/Details.java
+++ app/src/main/java/com/georgebrown/restaurant_guide/Details.java
@@ -3,6 +3,8 @@ package com.georgebrown.restaurant_guide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,6 +27,17 @@ public class Details extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+
+        ImageButton makeReview = findViewById(R.id.add_review_btn);
+
+        makeReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Details.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
