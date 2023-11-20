96d2757b25e85f00d142514a176d610b64563868
James MacAloney <121572130+JamesMaca@users.noreply.github.com>
2023-11-19 20:22:00 -0500

    added about us xml

--- app/src/main/java/com/georgebrown/restaurant_guide/Details.java
+++ app/src/main/java/com/georgebrown/restaurant_guide/Details.java
@@ -36,7 +36,7 @@ public class Details extends AppCompatActivity {
         makeReview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Details.this, MainActivity.class);
+                Intent intent = new Intent(Details.this, ReviewInterface.class);
                 startActivity(intent);
             }
         });
